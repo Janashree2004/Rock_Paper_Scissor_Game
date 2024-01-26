5eceb2a4690b8281efa0741f0cc46ca4aef4110d
Janashree <142415775+Janashree2004@users.noreply.github.com>
2024-01-26 15:46:50 +0530

    Add files via upload

---
+++ Stone_Paper_Scissor.py
@@ -0,0 +1,46 @@
+import random
+rock='''
+         _______
+_______/    _____)
+            (_____)
+            (______)
+______      (_____)
+      \_____(___)
+'''
+
+paper='''
+           ________
+_________/        __)_____
+                  (________)
+                  (_________)
+______            (________)
+      \___________(_______)
+'''
+
+scissor='''
+           ____
+_________/    __)_______
+            (____________)
+            (_____________)
+______      (_____)
+      \_____(___)
+'''
+n=[rock,paper,scissor]
+a=int(input("Enter the choice:- Type 0 for Rock, 1 for paper, 2 for scissor: "))
+if a>3 or a<0:
+    print("Invalid user, You Lose.")
+else:
+    print(n[a])
+    b=random.randint(0,3)
+    print("Computer choice:")
+    print(n[b])
+    if a==b:
+        print("It's A draw.")
+    elif a==0 and b==3:
+        print("You Win.")
+    elif a==3 and b==0:
+        print("You Lose.")
+    elif a>b:
+        print("You Win.")
+    elif a<b:
+        print("You Lose.")
